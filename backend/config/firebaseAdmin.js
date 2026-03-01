74f5a61e11571ffea6e9b60675305a941b83041a
Hadi Shah <hadishah.work@gmail.com>
2026-03-02 04:58:33 +0530

    chore: setup Firebase auth in backend

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,7 +1,11 @@
-import admin from "firebase-admin";
+// backend/config/firebaseAdmin.js
+
+const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+module.exports = admin;
